3a4f3f391d2d82ccc4689ff938dcb9118e3368ac
Tal Yaacovi <talyaacovi@gmail.com>
2018-03-24 07:18:28 -0700

    Started hangman game framework.

---
+++ app/components/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import { Home } from './Home';
+
+class Main extends Component {
+    render() {
+
+        return (
+        	<div className='row'>
+        		<div className='col-xs-12'>
+        			<h2>Welcome to the hangman game!</h2>
+        		</div>
+			</div>
+            )
+    }
+}
+
+
+export default Main;
---
+++ app/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './components/Main';
+
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+// var App = require('./components/App');
+
+ReactDOM.render(
+    <Main />,
+    document.getElementById('app')
+);
