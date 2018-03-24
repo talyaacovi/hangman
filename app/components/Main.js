85bf9e5c628b06452d3c4efbc668f4693a1d24bb
Tal Yaacovi <talyaacovi@gmail.com>
2018-03-24 18:56:38 -0500

    Keep track of number of guesses and display correct letters guessed on screen.

--- app/components/Main.js
+++ app/components/Main.js
@@ -4,7 +4,8 @@ import { Letter } from './Letter';
 class Main extends Component {
     constructor(props) {
     	super(props);
-    	this.state = { word: [], letters: [], game: false, correct: [], incorrect: [] };
+    	this.state = { word: [], letters: [], game: false, correct: [], incorrect: [], guesses: 0, status: null };
+    	this.gameStatus.bind(this);
     };
 
     componentWillMount() {
@@ -16,26 +17,37 @@ class Main extends Component {
     	let word = 'birthday';
     	word = word.split('');
     	this.setState({ game: true, word: word });
-    	let correct = <div></div>
-    	let incorrect = <div></div>
     }
     
-    checkGuess(letter) {
-    	let correct = this.state.correct;
-    	let incorrect = this.state.incorrect;
-
-    	if (this.state.word.includes(letter.toLowerCase())) {
-    		
-    		correct.push(letter);
-    		this.setState({ correct: correct });
-    		correct = this.state.correct.map(( letter, i) => <Letter letter={letter} key={i} />);
-    		console.log(correct);
+    gameStatus(guesses) {
+    	let statusMsg;
+    	if (this.state.correct.length === this.state.word.length) {
+    		statusMsg = 'You won in ' + guesses + ' guesses!';
+    		this.setState({ status: 'You won!' });
     	}
     	else {
-    		console.log('incorrect!');
-    		correct = this.state.correct.map(( letter, i) => <Letter letter={letter} key={i} />);
+    		statusMsg = 'You have ' + (10 - guesses) + ' guesses left!';
+    		this.setState({ status: statusMsg});
     	}
     }
+
+    checkGuess(letter) {
+    	if (this.state.word.includes(letter.toLowerCase())) {
+    		let currCorrect = this.state.correct;
+    		currCorrect.push(letter);
+    		this.setState({ correct: currCorrect});
+    	}
+    	else {
+    		let currIncorrect = this.state.incorrect;
+    		currIncorrect.push(letter);
+    		this.setState({ incorrect: currIncorrect});
+    	}
+    	let guesses = this.state.guesses + 1;
+    	this.setState({ guesses: guesses })
+    	this.gameStatus(guesses);
+    }
+
+
 
 
 
@@ -44,18 +56,37 @@ class Main extends Component {
     	
     	let letters = this.state.letters.map(( letter, i ) => <Letter onClick={this.checkGuess.bind(this)} letter={letter} key={i}/>);
     	let gameBoard;
-    	let correct = this.state.correct;
+    	let correct = this.state.correct.map(( letter, i) => <Letter letter={letter} key={i} />);
+    	let incorrect = this.state.incorrect.map(( letter, i) => <Letter letter={letter} key={i} />);
+    	let wordState = this.state.word.map(( letter, i ) => {
+    		if (this.state.correct.includes(letter.toUpperCase())) {
+    			return '  ' + letter + '  ';
+    		}
+    		else {
+    			return '   __  ';
+    		}
+    	});
+    	let status = this.state.status;
 
     	if (this.state.game) {
     		gameBoard = 
 	        		<div className='row'>
 	        			<div className='col-xs-3'>
+	        				{wordState}
+	        				<div>
+	        					{status}
+	        				</div>
 	        				<h3>Guess a letter</h3>
+	        				<p>Number of guesses: {this.state.guesses}</p>
 	        				{letters}
 	        			</div>
 	        			<div className='col-xs-3'>
 	        				<h3>Correct</h3>
 	        				{correct}
+	        			</div>
+	        			<div className='col-xs-3'>
+	        				<h3>Incorrect</h3>
+	        				{incorrect}
 	        			</div>
 	        		</div>
     	}
